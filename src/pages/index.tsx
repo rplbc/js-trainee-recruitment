b69583b850ff38609b77c8af8c6dc02c957bc83a
rplbc <radoslawlb@gmail.com>
2023-03-12 21:16:28 +0100

    add sass support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 import { LogoNerdbord } from "../components/LogoNerdbord";
 
-import styles from "../styles/index.module.css";
+import styles from "../styles/index.module.scss";
 
 export default function Home() {
   return (
