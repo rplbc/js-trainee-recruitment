85560448edbca84ea42109ed58bd383b14817233
rplbc <radoslawlb@gmail.com>
2023-03-20 23:40:17 +0100

    extract colors map

--- src/components/highlight/index.tsx
+++ src/components/highlight/index.tsx
@@ -1,14 +1,12 @@
+import { colors } from "@/styles/colors";
+
 export default function Highlight({
   color = "orange",
   ...props
 }: HighlightProps) {
-  const colors = {
-    orange: "var(--color-orange-500)",
-  };
-
   return <span style={{ color: colors[color] }} {...props} />;
 }
 
 export type HighlightProps = {
-  color?: "orange";
+  color?: keyof typeof colors;
 } & React.ComponentPropsWithoutRef<"span">;
